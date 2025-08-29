c28be1f293684f74b952efe5ccc1235dc5d50d12
Anchit <anchitkanaujia.98@gmail.com>
2025-08-30 01:43:39 +0530

    singin and singup backend

--- server.js
+++ server.js
@@ -10,12 +10,21 @@ connectDB();
 
 const app = express()
 
+
+app.use((req, res, next) => {
+    console.log(`📨 ${req.method} ${req.path} - ${new Date().toISOString()}`);
+    console.log('📋 Headers:', req.headers);
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/auth", require("./routes/v1/auth"));
 app.get('/', (req,res)=>{
     res.send("othello API is running");
 })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+ console.log(`🚀 Server running on port ${PORT}`);
